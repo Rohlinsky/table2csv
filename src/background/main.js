62efa8c4de2dfee57d2472a2b24e5158ab4eb79f
Denis Rohlinsky <denis.rohlinsky@gmail.com>
2020-11-14 23:43:27 +0200

    t2c: Store Data

--- src/background/main.js
+++ src/background/main.js
@@ -1,1 +1,17 @@
-// For background process
+// Set default data
+
+chrome.storage.sync.get('default_file_name', ({ default_file_name }) => {
+  if (!default_file_name) {
+	chrome.storage.sync.set({ 'default_file_name': 'table2csv' }, () => {
+		console.log('Basic data default_file_name was successfully set');
+	});
+  }
+});
+
+chrome.storage.sync.get('default_file_format', ({ default_file_format }) => {
+  if (!default_file_format) {
+	chrome.storage.sync.set({ 'default_file_format': 1 }, () => {
+		console.log('Basic data default_file_format was successfully set');
+	});
+  }
+});
--- src/options/js/main.js
+++ src/options/js/main.js
@@ -1,1 +1,29 @@
-// options js
+// Work with stored data
+
+chrome.storage.sync.get('default_file_name', ({ default_file_name }) => {
+  const file_name = document.getElementById('file_name');
+
+  if (default_file_name) {
+    file_name.value = default_file_name;
+  }
+
+  file_name.addEventListener('change', (e) => {
+    chrome.storage.sync.set({ 'default_file_name': file_name.value }, () => {
+      console.log('Data default_file_name was successfully set');
+    });
+  });
+});
+
+chrome.storage.sync.get('default_file_format', ({ default_file_format }) => {
+  const file_format = document.getElementById('file_format');
+
+  if (default_file_format) {
+    file_format.value = default_file_format;
+  }
+
+  file_format.addEventListener('change', (e) => {
+    chrome.storage.sync.set({ 'default_file_format': file_format.value }, () => {
+      console.log('Data default_file_format was successfully set');
+    });
+  });
+});
